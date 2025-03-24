ebc59da30e4b92038361b8894fb63cfad9b3b9b3
Biraj <biraj@outspeed.com>
2025-03-24 11:26:23 +0530

    fix: use createRoot instead of hydrateRoot to fix minified error

--- src/client/entry-client.tsx
+++ src/client/entry-client.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./base.css";
 import EntryCommon from "./entry-common";
 
-ReactDOM.hydrateRoot(
-  document.getElementById("root")!,
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <EntryCommon />
