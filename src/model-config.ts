cfb94be3cb6aa5e6ceeaaf6686dfe39e55231d5d
Biraj <biraj@outspeed.com>
2025-05-02 18:45:19 +0530

    feat: use transcription server event for both openai and outspeed

--- src/model-config.ts
+++ src/model-config.ts
@@ -16,13 +16,21 @@ export type SessionConfig = {
   turn_detection: {
     type: "server_vad" | "semantic_vad";
   };
+
+  input_audio_transcription?: {
+    model: string;
+  };
 };
 
 type ModelValue = {
   label: string;
   voices: string[];
   sessionConfig: SessionConfig;
+  transcriptionModels: string[];
 };
+
+const OUTSPEED_TRANSCRIPTION_MODELS = ["whisper-v3-turbo"];
+const OPENAI_TRANSCRIPTION_MODELS = ["gpt-4o-transcribe", "gpt-4o-mini-transcribe", "whisper-1"];
 
 const OUTSPEED_ORPHEUS_VOICES = ["tara", "leah", "jess", "leo", "dan", "mia", "zac", "zoe", "julia"];
 const OPENAI_VOICES = ["alloy", "ash", "ballad", "coral", "echo", "sage", "shimmer", "verse"];
@@ -31,6 +39,7 @@ export const models: Record<ModelName, ModelValue> = {
   "Orpheus-3b": {
     label: "Orpheus 3b",
     voices: OUTSPEED_ORPHEUS_VOICES,
+    transcriptionModels: OUTSPEED_TRANSCRIPTION_MODELS,
     sessionConfig: {
       model: "Orpheus-3b",
       modalities: ["audio", "text"],
@@ -41,11 +50,15 @@ export const models: Record<ModelName, ModelValue> = {
       turn_detection: {
         type: "server_vad",
       },
+      input_audio_transcription: {
+        model: "whisper-v3-turbo",
+      },
     },
   },
   "gpt-4o-realtime-preview-2024-12-17": {
     label: "GPT-4o Realtime",
     voices: OPENAI_VOICES,
+    transcriptionModels: OPENAI_TRANSCRIPTION_MODELS,
     sessionConfig: {
       model: "gpt-4o-realtime-preview-2024-12-17",
       modalities: ["audio", "text"],
@@ -63,6 +76,7 @@ export const models: Record<ModelName, ModelValue> = {
   "gpt-4o-mini-realtime-preview-2024-12-17": {
     label: "GPT-4o Mini Realtime",
     voices: OPENAI_VOICES,
+    transcriptionModels: OPENAI_TRANSCRIPTION_MODELS,
     sessionConfig: {
       model: "gpt-4o-mini-realtime-preview-2024-12-17",
       modalities: ["audio", "text"],
